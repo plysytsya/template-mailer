446bacad16c98ee83c6ea3fb0b96a6a57f2a2649
Pavlo Lysytsya <pavlo.lysytsya@outfittery.de>
2019-12-07 21:42:58 +0100

    adds logger to SMTPClient reformats code

--- src/template_mailer/smtp_client.py
+++ src/template_mailer/smtp_client.py
@@ -2,32 +2,53 @@ import smtplib
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 import os
+import logging
+
+logging.basicConfig(level=logging.INFO)
 
 
 class SMTPClient:
-    def __init__(self, SMTP_HOST=None, SMTP_PORT=None, EMAIL_USER=None, EMAIL_PASSWORD=None):
-        self.smtp = os.environ.get("SMTP_HOST", default=SMTP_HOST)
-        self.port = os.environ.get("SMTP_PORT", default=SMTP_PORT)
-        self.username = os.environ.get("EMAIL_USER", default=EMAIL_USER)
-        self.password = os.environ.get("EMAIL_PASSWORD", default=EMAIL_PASSWORD)
+    def __init__(
+        self,
+        smtp_host=None,
+        smtp_port=None,
+        email_user=None,
+        email_password=None,
+        logger=logging.getLogger(),
+    ):
+        self.smtp_host = os.environ.get("SMTP_HOST", default=smtp_host)
+        self.smtp_port = os.environ.get("SMTP_PORT", default=smtp_port)
+        self.username = os.environ.get("EMAIL_USER", default=email_user)
+        self.password = os.environ.get("EMAIL_PASSWORD", default=email_password)
         self.server = None
+        self.logger = logger
 
     def send(self, recipient, subject, html):
         self.connect()
         message = self.build_message(html)
         self.server.sendmail(self.username, recipient, message)
+        self.logger.info("Sent email to %s" % recipient)
         self.server.quit()
 
     def connect(self):
         # Passing smtp-host two times to avoid python 3.7 bug.
-        self.server = smtplib.SMTP(self.smtp)
-        self.server.connect(self.smtp, self.port)
+        self.logger.info(
+            "Connecting to %s with tls on port %s" % (self.smtp_host, self.smtp_port)
+        )
+        self.server = smtplib.SMTP(self.smtp_host)
+        self.server.connect(self.smtp_host, self.smtp_port)
         self.server.ehlo()
         self.server.starttls()
         self.server.login(self.username, self.password)
+        self.logger.info("Established tls-connection with %s" % self.smtp_host)
+
+    def disconnect(self):
+        self.logger.info("Disconnecting from %s" % self.smtp_host)
+        self.server.quit()
+        self.logger.info("Disconnected from %s" % self.smtp_host)
 
     def build_message(self, html):
-        message = MIMEMultipart('alternative')
-        mime_text = MIMEText(html, 'html')
+        message = MIMEMultipart("alternative")
+        mime_text = MIMEText(html, "html")
         message.attach(mime_text)
         return message.as_string()
